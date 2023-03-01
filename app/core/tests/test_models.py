679e14ba7afc9efd8addb1af22aa0a59d2990ef1
Moinak <moinak.dey8@gmail.com>
2023-03-01 19:12:30 +0530

    superuser creation and login to admin portal

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -35,5 +35,3 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
-        self.assertTrue(user.is_admin)
-        self.assertTrue(user.is_active)
