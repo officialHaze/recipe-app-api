13ea76052bbaeff9315c05112fe0ed383ebda9ac
Moinak <moinak.dey8@gmail.com>
2023-03-01 17:31:28 +0530

    added features to user model

--- app/core/models.py
+++ app/core/models.py
@@ -14,11 +14,21 @@ def normalized_email(email):
 class UserManager(BaseUserManager):
 
     def create_user(self, email, password=None, **extra_fields):
+        if not email:
+             raise ValueError('User must have an email address')
         user = self.model(email=normalized_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
 
         return user
+
+    def create_superuser(self, email, password):
+         user = self.create_user(email, password)
+         user.is_superuser = True
+         user.is_staff = True
+         user.save(using=self._db)
+
+         return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
